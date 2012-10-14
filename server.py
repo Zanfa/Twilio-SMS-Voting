0e8dfabbbe73f6d98adec141cb6644f64f2bdd3e
Vahur Roosimaa <vahur@scopely.com>
2012-10-14 00:46:56 -0700

    Twilio response support

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, Response
+from flask.ext.pymongo import PyMongo
+
+# phone number is (424) 231-5779
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://user:password@ds039717.mongolab.com:39717/heroku_app8402029"
 
-@app.route('/')
+mongo = PyMongo(app)
+
+@app.route('/', methods=['get', 'post'])
 def hello():
-    return 'Hello World!'
+    return Response("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Sms>Hello, Mobile Monkey</Sms></Response>",
+        mimetype="text/xml")
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
